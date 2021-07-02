556365a80f749589adacbed2b4e5f67c1459c074
OBess <dima.suschenko03@gmail.com>
2021-07-02 12:07:27 +0300

    Added default constructor and destructor. Also, added fields of size, capacity and (dinamic array) table.

--- include/hash_table.hpp
+++ include/hash_table.hpp
@@ -1,6 +1,26 @@
+// C++
+#include <iostream>
+#include <vector>
 
-template<class Key, class Data, int Size = 30>
+template <class Key, class Data, size_t Size = 30,
+          size_t HashFunc = [](const Key &key, const size_t &size)
+          { return key % size; }>
 class hash_table
 {
+public:
+   hash_table()
+   {
+      table = new Data[Size];
+   }
 
+   ~hash_table() noexcept
+   {
+      delete[] table;
+   }
+
+private:
+   size_t m_capacity = Size;
+   size_t m_size = 0;
+
+   Data *table = nullptr;
 };
