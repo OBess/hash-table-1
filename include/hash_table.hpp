65ea1f0f2333afd8ac89a928bfcb04debc6f26b7
OBess <dima.suschenko03@gmail.com>
2021-07-02 12:15:29 +0300

    Changed pointer table to vector(matrix) and remove destructor

--- include/hash_table.hpp
+++ include/hash_table.hpp
@@ -5,25 +5,20 @@
 #include <vector>
 
 template <class Key, class Data, size_t Size = 30,
-          std::function<const Key &key, const size_t &size> HashFunc = [](const Key &key, const size_t &size)
+          std::function<const Key & key, const size_t & size> HashFunc = [](const Key &key, const size_t &size)
           { return key % size; }>
 class hash_table
 {
 public:
    hash_table()
    {
-      table = new Data[Size];
-   }
-
-   ~hash_table() noexcept
-   {
-      delete[] table;
+      table.reserve(Size);
    }
 
 private:
    size_t m_capacity = Size;
    size_t m_size = 0;
 
-   Data *table = nullptr;
-   std::function<const Key &key, const size_t &size> m_hashFunc = HashFunc;
+   std::vector<std::vector<Data>> table;
+   std::function<const Key & key, const size_t &size> m_hashFunc = HashFunc;
 };
